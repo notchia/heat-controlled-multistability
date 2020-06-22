572ddb5d41878b821407662a6eabbbdcee089124
lkorpas <lucia.korpas@gmail.com>
2020-06-22 19:55:46 -0400

    update axis scaling, add axis labels

--- modules/paraview_phase_diagram.py
+++ modules/paraview_phase_diagram.py
@@ -11,17 +11,19 @@ pv._DisableFirstRenderCameraReset()
 
 
 def render_surface(fname):
-    font_size = 36
-    ColormapScale = 20.0
-    time_max      = 0.4
-    n_unit_max    = 300
+    fontSize = 16
+    textColor = [0,0,0] #black
+    
+    axisLabels = [r'$h$ (mm)', r'$\theta_L$ ($^{\circ}$)', r'$T$ ($^{\circ}$C)']
+    axisLocations = [(0.65, 0.05), (0.15, 0.1), (0.0, 0.5)] # (x,y) from bottom left
 
-    warpScale=1.0
-    gridScale_x=0.2
+    gridScale = [45, 2, 1]
+    figureSize = [1200, 900]
     
     # Import VTK file and initialize render view
     boundaryVTK = pv.LegacyVTKReader(FileNames=[fname])
     renderView = pv.GetActiveViewOrCreate('RenderView')
+
     
     boundaryVTKDisplay = pv.Show(boundaryVTK, renderView)
     boundaryVTKDisplay.Representation = 'Surface'
@@ -47,33 +49,24 @@ def render_surface(fname):
     triangleMeshDisplay.SetScalarBarVisibility(renderView, True)
     renderView.Update()
     
-    
-    warpByScalar1 = pv.WarpByScalar(Input=triangleMesh)
-    warpByScalar1.ScaleFactor = warpScale
-    
-    warpByScalar1Display = pv.Show(warpByScalar1, renderView)
-    warpByScalar1Display.Representation = 'Surface'
-
-    pv.Hide(triangleMesh, renderView)
-    
-    # show color bar/color legend
-    warpByScalar1Display.SetScalarBarVisibility(renderView, True)
-    warpByScalar1Display.Scale = [1.0, gridScale_x, 1.0]
-
     # update the view to ensure updated data information
     renderView.Update()    
     
+    loopSubdivision1Display = triangleMeshDisplay
+    #loopSubdivision1Display = warpByScalar1Display # DELETE THIS ONCE LOOP SUBDIVISON WORKS
+    
     '''
     # create a new 'Loop Subdivision' filter to increases the mesh granularity
-    loopSubdivision1 = pv.LoopSubdivision(Input=warpByScalar1)
+    loopSubdivision1 = pv.LoopSubdivision(Input=triangleMesh)
     loopSubdivision1.NumberofSubdivisions = 2
     
     # Update view with new finer mesh; hide original mesh
     loopSubdivision1Display = pv.Show(loopSubdivision1, renderView)
     loopSubdivision1Display.Representation = 'Surface'
-    pv.Hide(warpByScalar1, renderView) 
+    pv.Hide(triangleMeshDisplay, renderView) 
     loopSubdivision1Display.SetScalarBarVisibility(renderView, True)
     renderView.Update()
+    '''
     
     # Modify colorbar
     colorbar = pv.GetScalarBar(scalarsLookupTable, renderView)
@@ -81,90 +74,90 @@ def render_surface(fname):
     # Modify colorbar properties
     colorbar.AutoOrient = 0
     colorbar.Orientation = 'Horizontal'
-    colorbar.Title = 'Rotational angle (deg)'
-    colorbar.TitleFontSize = font_size
-    colorbar.LabelFontSize = font_size
+    colorbar.Title = 'Boundary "value"'
+    colorbar.TitleFontSize = fontSize
+    colorbar.LabelFontSize = fontSize
     colorbar.RangeLabelFormat = '%-#1.2f'
-    colorbar.UseCustomLabels = 1
-    colorbar.CustomLabels = [-50.0, 0.0, 50.0]
+    colorbar.TitleColor = textColor
+    colorbar.LabelColor = textColor
+        
+    #colorbar.UseCustomLabels = 1
+    #colorbar.CustomLabels = [-50.0, 0.0, 50.0]
     colorbar.AddRangeLabels = 0
     
     # Rescale transfer function
-    scalarsLookupTable.RescaleTransferFunction(-ColormapScale, ColormapScale)
+    #scalarsLookupTable.RescaleTransferFunction(-ColormapScale, ColormapScale)
     
     # Get opacity transfer function/opacity map for 'point_scalars'
     scalarsPWF = pv.GetOpacityTransferFunction('point_scalars')
     
     # change scalar bar placement
     colorbar.WindowLocation = 'AnyLocation'
-    colorbar.Position = [0.08722699983649515, 0.7587948450017415]
+    colorbar.Position = [0.1, 0.90]
     colorbar.ScalarBarLength = 0.2
     
     renderView.ResetCamera()
     
     # Modify axes grid properties 
     renderView.AxesGrid.Visibility = 1
-    renderView.AxesGrid.GridColor = [0.0, 0.0, 0.0]
-    renderView.AxesGrid.ZLabelOpacity = 0.0
+    renderView.AxesGrid.GridColor = textColor
+    renderView.AxesGrid.ZLabelOpacity = 1
     
-    # Reset view to fit data bounds
-    renderView.ResetCamera(1.0, 20.0, 0.0, 0.600000023842, -0.578562915325, 1.03565680981)
-
-    loopSubdivision1Display.Scale = [1.0, gridScale_x, 1.0]
+    loopSubdivision1Display.Scale = gridScale
     
-    renderView.AxesGrid.AxesToLabel = 24    
-    renderView.AxesGrid.XTitle = ''
-    renderView.AxesGrid.YTitle = ''
-    renderView.AxesGrid.ZTitle = ''
-    renderView.AxesGrid.XTitleFontSize = font_size
-    renderView.AxesGrid.YTitleFontSize = font_size
-    renderView.AxesGrid.ZTitleFontSize = font_size
-    renderView.AxesGrid.XLabelFontSize = font_size
-    renderView.AxesGrid.YLabelFontSize = font_size
-    renderView.AxesGrid.ZLabelFontSize = font_size
-    renderView.AxesGrid.XAxisUseCustomLabels = 1
-    renderView.AxesGrid.XAxisLabels = [1,10,20,int(n_unit_max)]
-    renderView.AxesGrid.YAxisUseCustomLabels = 1
-    renderView.AxesGrid.YAxisPrecision = 1
-    renderView.AxesGrid.YAxisNotation = 'Fixed'
-    renderView.AxesGrid.YAxisLabels = [0,time_max/3.,time_max*2./3.,time_max]
+    #renderView.AxesGrid.AxesToLabel = 3
+    renderView.AxesGrid.XTitle = ''#axisLabels[0]
+    renderView.AxesGrid.YTitle = ''#axisLabels[1]
+    renderView.AxesGrid.ZTitle = ''#axisLabels[2]
+    renderView.AxesGrid.XTitleFontSize = 2*fontSize
+    renderView.AxesGrid.YTitleFontSize = 2*fontSize
+    renderView.AxesGrid.ZTitleFontSize = 2*fontSize
+    renderView.AxesGrid.XLabelFontSize = fontSize
+    renderView.AxesGrid.YLabelFontSize = fontSize
+    renderView.AxesGrid.ZLabelFontSize = fontSize
+    renderView.AxesGrid.XTitleColor = textColor
+    renderView.AxesGrid.YTitleColor = textColor
+    renderView.AxesGrid.ZTitleColor = textColor
+    renderView.AxesGrid.XLabelColor = textColor
+    renderView.AxesGrid.YLabelColor = textColor
+    renderView.AxesGrid.ZLabelColor = textColor
+    renderView.AxesGrid.ShowGrid = 1
+    renderView.AxesGrid.DataScale = gridScale
 
-    renderView.AxesGrid.DataScale = [1.0, gridScale_x, 1.0]
+    renderView.Update() 
     
     # Display and format labels
     text1 = pv.Text()
-    text1.Text = 'Unit index'
-    
+    text1.Text = axisLabels[0]
     text1Display = pv.Show(text1, renderView)
-    text1Display.Color = [0.0, 0.0, 0.0]
+    text1Display.Color = textColor
     text1Display.WindowLocation = 'AnyLocation'
-    text1Display.Position = [0.703024, 0.077775]
-    text1Display.FontSize = 8
+    text1Display.Position = axisLocations[0]
+    text1Display.FontSize = fontSize
     
     text2 = pv.Text()
-    text2.Text = 'Time (s)'
-    
+    text2.Text = axisLabels[1]
     text2Display = pv.Show(text2, renderView)
-    text2Display.Color = [0.0, 0.0, 0.0]
+    text2Display.Color = textColor
     text2Display.WindowLocation = 'AnyLocation'
-    text2Display.Position = [0.199768, 0.308965]
-    text2Display.FontSize = 8
+    text2Display.Position = axisLocations[1]
+    text2Display.FontSize = fontSize
     
     text3 = pv.Text()
-    text3.Text = 'Rotational\nangle'
-    
+    text3.Text = axisLabels[2]
     text3Display = pv.Show(text3, renderView)   
-    text3Display.Color = [0.0, 0.0, 0.0]
+    text3Display.Color = textColor
     text3Display.WindowLocation = 'AnyLocation'
-    text3Display.Position = [0.026744, 0.627147]
-    text3Display.FontSize = 8
-      
+    text3Display.Position = axisLocations[2]
+    text3Display.FontSize = fontSize
+    
     # current camera placement for renderView
-    renderView.CameraPosition = [-28.675884090976982, -29.201098793489997, 37.491226623005886]
-    renderView.CameraFocalPoint = [-0.9752431013731799, 4.8856287692666145, 11.313734940891882]
-    renderView.CameraViewUp = [0.2951277177032757, 0.4194402496929348, 0.8584692814427224]
-    renderView.CameraParallelScale = 10.937157944358066    
-    '''
+    renderView.CameraPosition = [-28.6, -29.2, 37.4]
+    renderView.CameraFocalPoint = [-0.97, 4.88, 11.31]
+    renderView.CameraViewUp = [0.29, 0.41, 0.85]
+    renderView.CameraParallelScale = 1   
+
+    pv.GetRenderView().ViewSize = figureSize
 
     renderView.Update()    
 
@@ -174,6 +167,10 @@ def render_surfaces(fnameList):
         render_surface(fname)
         
     pv.Render()
+    
+'''To do: add isotherm'''
+#def render_isotherm(fname):
+    
 
 #--------------------------------------------------------------------------
 
@@ -186,3 +183,7 @@ if __name__ == "__main__":
         fname = os.path.join(basepath, '20200422_boundaryData_{0}.vtk'.format(value))
         fnameList.append(fname)
     render_surfaces(fnameList)
+    pv.SaveScreenshot(os.path.join(basepath,'paraview.png'),pv.GetActiveView(),
+                      TransparentBackground=1)
+    
+    
